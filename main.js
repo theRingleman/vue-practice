8d1442e8858d713fff28dd1d4139f6f190054cb4
Sam Ringleman <sam.rinleman@gmail.com>
2017-07-07 21:30:09 -0600

    add some more testing functions from the vue guide

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
-new Vue({
-  el: '#root',
-
+var app1 = new Vue({
+  el: '#app1',
   data: {
     message: 'Hello World'
   }
+})
+
+var app2 = new Vue({
+	el: '#app2',
+	data: {
+		message: 'This is a test!',
+		title: 'This is another test of the title system!'
+	}
+})
+
+var app3 = new Vue ({
+	el: '#app3',
+	data: {
+		seen: true
+	}
+})
+
+var app4 = new Vue({
+	el: '#app4',
+	data: {
+		todos: [
+			{text: 'Learn Javascript'},
+			{text: 'Learn Vue'},
+			{text: 'Build awesome things!'}
+		]
+	}
 })
